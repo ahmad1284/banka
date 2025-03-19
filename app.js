dda7e2f7716accd0fdbca9e42a5adee46db622ca
Ahmad Abdullah <ahmadnofly@gmail.com>
2025-03-19 12:31:13 +0000

    START OVER

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 // ---------------------------------------------------------------------------
 
 // const serverUrl = 'https://kigombo-be-7855b43eb5fb.herokuapp.com/api';
-const serverUrl = 'https://kigombobeta-f9f2f4b59adf.herokuapp.com/api' ;
+// const serverUrl = 'https://kigombobeta-f9f2f4b59adf.herokuapp.com/api' ;
+
+const serverUrl = 'http://localhost:5000/api';
 const storageKey = 'savedAccount';
 
 // ---------------------------------------------------------------------------
